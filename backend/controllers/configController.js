b639eec9ba3f0894d56bbae6b99fd8645415b748
Juvielone Lagos <62714904+juvielone@users.noreply.github.com>
2023-02-04 01:06:49 +0800

    Created Controller for config

--- backend/controllers/configController.js
+++ backend/controllers/configController.js
@@ -2,34 +2,31 @@
 // express routes and passing them to our express error handlers
 const asyncHandler = require("express-async-handler");
 const AdminModel = require("../models/admin/adminModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // Create Admin Dev
 const createAdmin = asyncHandler(async (req, res) => {
-  const { userId, password, phone, service } = req.body;
+  const { userId, password, role } = req.body;
+  //Checks if user exists
+  const adminExists = await AdminModel.findOne({ userId });
 
+  if (adminExists) {
+    res.status(400);
+    throw new Error("Admin already registered");
+  }
+  //Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
   // Create Admin
   const adminUser = await AdminModel.create({
     userId,
-    password,
-    phone,
-    service: [
-      { service_name: "Rabies Vaccine", service_desc: "Prevent rabies" },
-      { service_name: "Grooming", service_desc: "Haircut, Claw Maintenance" },
-      {
-        service_name: "Day Care",
-        service_desc: "Dog staycation with 3 heavy meals",
-      },
-    ],
+    password: hashedPassword,
+    role,
   });
 
-  // Console logs
   if (adminUser) {
-    res.status(201).json({
-      userId: adminUser.userId,
-      password: adminUser.password,
-      phone: adminUser.phone,
-      service: adminUser.service,
-    });
+    res.status(201).json(adminUser);
   } else {
     res.status(400);
     throw new Error("Invalid Admin Data");
@@ -37,9 +34,35 @@ const createAdmin = asyncHandler(async (req, res) => {
 });
 
 // Login Admin
+// @desc    Authenticate Admin
+// @acess   Public
+const loginAdmin = asyncHandler(async (req, res) => {
+  const { userId, password } = req.body;
+
+  const adminUser = await AdminModel.findOne({ userId });
+
+  if (adminUser && (await bcrypt.compare(password, adminUser.password))) {
+    res.status(201).json({
+      userId: adminUser.userId,
+      role: adminUser.role,
+      token: generateToken(adminUser._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Credentials");
+  }
+});
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   createAdmin,
+  loginAdmin,
 };
 
 // Update Admin
